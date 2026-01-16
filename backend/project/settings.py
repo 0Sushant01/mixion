1f63f6fe1e18390fb2215826ce7ec8ad04f19197
0Sushant01 <0sushantmore00@gmail.com>
2026-01-16 21:02:51 +0530

    db added

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -143,6 +143,8 @@ REST_FRAMEWORK = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://0.0.0.0:3000",
+    "https://0.0.0.0:3000"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
